927ce588523a6e3c20e94593c3e5eeea98cd4926
Erick Sulca <ejyp259@hotmail.com>
2021-09-24 18:22:45 -0500

    Se agrega la funcion de guardar imagen y se almacena en sistema y se agrega en modelo

--- app/views.py
+++ app/views.py
@@ -8,11 +8,33 @@ import cv2
 import threading
 from .models import *
 from .forms import *
+import os 
 
 def Home(request):
 	if request.method=='GET':
 		form = ImagenForm()
 		return render(request,'base.html',{'form':form})
 	if request.method=='POST':
-		pass
-		return render(request,'base.html',{'form':form})
+		Datos = request.POST
+		form = ImagenForm(request.POST, request.FILES)
+		if form.is_valid():
+			oImagen = Imagen()
+			oImagen.descripcion = Datos["descripcion"]
+			oImagen.save()
+			oImagen.imagen = form.cleaned_data["imagen"]
+			oImagen.save()
+			#try:
+			ruta_imagen = '.'+str(oImagen.imagen.url)
+			img_bgr = cv2.imread(ruta_imagen, cv2.IMREAD_UNCHANGED)
+			#img_bgr = cv2.imread('/home/erick/Software/B&B2Color/convert2color/foto.jpeg')
+			if img_bgr.ndim == 2:
+				img_color = cv2.cvtColor(img_bgr, cv2.COLOR_GRAY2BGR)  #G
+				print("Entro en gris")
+			else:
+				print("Entro en BGR")
+				img_color = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB) #RGB
+			cv2.imwrite('media/color.jpeg',img_color)
+			oImagen.save()
+			
+
+		return render(request,'base.html',{'form':form, 'oImagen':oImagen})
--- convert2color/settings.py
+++ convert2color/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,12 +120,23 @@ USE_L10N = True
 USE_TZ = False
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+]
+
+
+STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_URL = '/media/'
